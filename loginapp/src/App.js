cba96f1f65de2b59d8e810e6a5b9903f55e69963
piyushsingh82 <piyushsingh2@gmail.com>
2021-05-16 19:45:36 +0530

    updated code

--- loginapp/src/App.js
+++ loginapp/src/App.js
@@ -1,18 +1,16 @@
 // import logo from './logo.svg';
 import './App.css';
-import Home from "./components/Home";
-import Listing from "./components/Listing";
-import About from "./components/About";
+// import Homepage from "./components/Home";
+// import Listing from "./components/Listing";
+// import About from "./components/About";
 import Header from "./components/Header";
 import Login from "./components/Login";
 function App() {
   return (
     <div className="App">
       <Header/>
-       <Login/>
-       <Home/>
-      <Listing/>
-      <About/>
+      <Login/>
+       
     </div>
   );
 }
--- loginapp/src/index.js
+++ loginapp/src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import reactrouter from 'react-router';
 
 ReactDOM.render(
-  <React.StrictMode>
+<React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
